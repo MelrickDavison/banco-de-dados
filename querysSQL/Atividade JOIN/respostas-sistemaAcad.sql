548a33f7c80e19b1c39d6d4d9d548b7a17daf3e1
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-08-22 21:51:05 -0300

    Update respostas-sistemaAcad.sql

--- querysSQL/Atividade JOIN/respostas-sistemaAcad.sql
+++ querysSQL/Atividade JOIN/respostas-sistemaAcad.sql
@@ -3,7 +3,7 @@ SELECT nome FROM disciplina WHERE id IN (
 SELECT FK_id_disciplina FROM alunodisciplina WHERE FK_matricula = (
 SELECT matricula FROM aluno WHERE nome = "David"));
 
-# Utilizando o JOIN
+# Utilizando o JOIN - NICE
 SELECT d.nome FROM aluno a INNER JOIN alunoDisciplina ad 
 ON ad.FK_matricula = a.matricula INNER JOIN disciplina d 
 ON d.id = ad.FK_id_disciplina WHERE a.nome = "David";
@@ -14,7 +14,7 @@ SELECT FK_id_disciplina FROM alunodisciplina WHERE FK_matricula = (
 SELECT matricula FROM aluno WHERE nome = "David")) AND FK_id_professor = (
 SELECT siape FROM professor WHERE nome = "João");
 
-# Utilizando o JOIN
+# Utilizando o JOIN - NICE
 SELECT d.nome as nomeDisciplina FROM disciplina d INNER JOIN alunodisciplina ad
 ON d.id = ad.FK_id_disciplina INNER JOIN aluno a 
 ON a.matricula = ad.FK_matricula INNER JOIN professor p 
@@ -28,7 +28,7 @@ SELECT FK_id_curso FROM cursodisciplina WHERE FK_id_disciplina IN (
 SELECT id FROM disciplina WHERE FK_id_professor = (
 SELECT siape FROM professor WHERE nome = "João")));
 
-# Utilizando o JOIN
+# Utilizando o JOIN - ERRO
 SELECT c.nome AS nomeCurso FROM curso c INNER JOIN cursodisciplina cd 
 ON c.id = cd.FK_id_curso INNER JOIN disciplina d
 ON d.id = cd.FK_id_disciplina INNER JOIN professor p
@@ -39,12 +39,10 @@ SELECT FK_id_disciplina, FK_matricula, nota FROM alunodisciplina WHERE FK_id_dis
 SELECT id FROM disciplina WHERE FK_id_professor = (
 SELECT siape FROM professor WHERE nome = "João")) GROUP BY FK_id_disciplina HAVING MAX(nota);
 
-# Utilizando o JOIN
-SELECT a.nome AS nomeAluno, d.nome AS nomeDisciplina, ad.nota FROM aluno a INNER JOIN alunodisciplina ad
-ON a.matricula = ad.FK_matricula INNER JOIN disciplina d 
+# Utilizando o JOIN - ERRO
+SELECT d.nome AS nomeDisciplina, ad.FK_matricula AS matricula, ad.nota FROM alunodisciplina ad INNER JOIN disciplina d 
 ON d.id = ad.FK_id_disciplina INNER JOIN professor p 
-ON p.siape = d.FK_id_professor 
-WHERE p.nome = "João" GROUP BY ad.FK_id_disciplina HAVING MAX(ad.nota);
+ON p.siape = d.FK_id_professor WHERE p.nome = "João" GROUP BY ad.FK_id_disciplina HAVING MAX(ad.nota);
 
 #Quais disciplinas os estudantes David e Henrique cursam juntos?
 SELECT nome FROM disciplina WHERE id IN (
@@ -52,6 +50,15 @@ SELECT FK_id_disciplina FROM alunodisciplina WHERE FK_matricula = (
 SELECT matricula FROM aluno WHERE nome = "David")) AND id IN (SELECT FK_id_disciplina FROM alunodisciplina WHERE FK_matricula = (
 SELECT matricula FROM aluno WHERE nome = "Henrique"));
 
+#Usando JOIN - ERRO
+SELECT * FROM disciplina d INNER JOIN alunodisciplina ad
+ON d.id = ad.FK_id_disciplina INNER JOIN aluno a 
+ON ad.FK_matricula = a.matricula  WHERE a.nome = "Henrique" AND a.nome = "David";
+
 #Quais os estudantes que cursam Integrado em Informática?
 SELECT nome FROM aluno WHERE FK_id_curso = (
 SELECT id FROM curso WHERE nome = "Integrado em Informática");
+
+#Utilizando o JOIN - NICE
+SELECT a.nome FROM aluno a INNER JOIN curso c 
+ON a.FK_id_curso = c.id WHERE c.nome = "Integrado em Informática";
