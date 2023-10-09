9fdc24f9eb4b285b112df8985d14a73a7e9061e5
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-10-08 23:02:24 -0300

    Update instudo_querys-gui.sql

--- 3°bimestre/instudo_querys-gui.sql
+++ 3°bimestre/instudo_querys-gui.sql
@@ -14,17 +14,17 @@ INSERT INTO perfilprofessores VALUES ('Nenelson','NelSon','2010-01-15', 19000),(
 
 INSERT INTO storysaluno VALUES (1, 'fabricio_arcanjo.09', 'foto', '2023-10-08');
 
-INSERT INTO storysaempresa VALUES (12, 'cesmac.01', 'foto', '2023-10-08');
+INSERT INTO storysempresa VALUES (12, 'cesmac.01', 'foto', '2023-10-08');
 
 INSERT INTO storysprofessores VALUES (13, 'NelSon', 'foto', '2023-10-08');
 
-INSERT INTO amizadesalunos VALUES ('fabricio_arcanjo.09', 'gabrielLuiz'), ('gabrielLuiz', 'Kemylly._'), ('melrickkj', 'Qemmili13'), ('fabricio_arcanjo.09', 'Kemylly._'), ('fabricio_arcanjo.09', 'melrickkj');
+INSERT INTO amizadesalunos VALUES ('fabricio_arcanjo.09', 'gabrielLuiz', "Bora fazer Banco?"), ('gabrielLuiz', 'Kemylly._', ""), ('melrickkj', 'Qemmili13', ""), ('fabricio_arcanjo.09', 'Kemylly._', "sksksksksk"), ('fabricio_arcanjo.09', 'melrickkj', "Oi, tudo bem?");
 
-INSERT INTO amizadesalunoprofessor VALUES ('fabricio_arcanjo.09', 'NelSon'), ('gabrielLuiz', 'NelSon'), ('melrickkj', 'dhs.lima'), ('fabricio_arcanjo.09', 'dhs.lima'), ('fabricio_arcanjo.09', 'pabloDosTeclados'), ('Kemylly._', 'dhs.lima');
+INSERT INTO amizadesalunoprofessor VALUES ('fabricio_arcanjo.09', 'NelSon', ""), ('gabrielLuiz', 'NelSon', ""), ('melrickkj', 'dhs.lima', "Bom dia"), ('fabricio_arcanjo.09', 'dhs.lima', "Oii"), ('fabricio_arcanjo.09', 'pabloDosTeclados', "Me tira uma dúvida"), ('Kemylly._', 'dhs.lima', "Bora adiar a atividade bora?");
 
-INSERT INTO amizadesprofessorempresa VALUES ('NelSon', 'cesmac.01'), ('NelSon', 'ufaldodeboche'), ('dhs.lima', 'ufaldodeboche'), ( 'pabloDosTeclados', 'ufaldodeboche');
+INSERT INTO amizadesprofessorempresa VALUES ('NelSon', 'cesmac.01', "Oi"), ('NelSon', 'ufaldodeboche', "Olá"), ('dhs.lima', 'ufaldodeboche', "Bom dia, segue os requerimentos"), ( 'pabloDosTeclados', 'ufaldodeboche', "");
 
-INSERT INTO amizadesalunoempresa VALUES ('fabricio_arcanjo.09', 'cesmac.01'), ('melrickkj', 'ufaldodeboche'), ('fabricio_arcanjo.09', 'ufaldodeboche'), ( 'Kemylly._', 'ufaldodeboche');
+INSERT INTO amizadesalunoempresa VALUES ('fabricio_arcanjo.09', 'cesmac.01',""), ('melrickkj', 'ufaldodeboche', ""), ('fabricio_arcanjo.09', 'ufaldodeboche', ""), ( 'Kemylly._', 'ufaldodeboche', "");
 
 INSERT INTO posts VALUES (1, "texto", "Sono, quero dormir", "Dois", "2021-09-30", 10), (2, 'video', 'IFAL - Jogos internos', 'amoo', '2022-08-23', 19), (3, "Foto", 'Vai Corinthians', 'eca', '2023-10-04', 109), (10, 'texto', "Vagas Abertas", 'Onde me inscrevo?', '2023-01-20', 5000), (20, 'foto', 'Prêmio de Xadrez, Pódio', 'Parabéns','2021-09-30', 1), (13, 'texto', 'Estou com tempo,  mas sem coragem', '*choro*', '2023-10-02', 32), (21, 'foto', "", 'Felicidades!', '2023-09-25', 109);
 
@@ -46,11 +46,11 @@ INSERT INTO empresaAluno VALUES ('UFAL_oficial','LLL_Fabricio', 3), ( 'CesmacAL'
 
 INSERT INTO empresaProfessor VALUES (1, 'UFAL_oficial','Nenelson'), (1, 'UFAL_oficial', 'dhs.lima');
 
-INSERT INTO materias VALUES("POO-01", "Programação Orientada a Objetos", "Class"), ("BD-01", "Banco de Dados", "Normalização"),("REDES-01", "Introdução a Redes", "Camadas de Aplicação");
+INSERT INTO materias VALUES("POO-01", "Programação Orientada a Objetos"), ("BD-01", "Banco de Dados"),("REDES-01", "Introdução a Redes");
 
 INSERT INTO atividades VALUES(1, "POO-01", 2, "2023-09-08", "Atividade de Classes", 100, "B, C, E, F"), (2, "BD-01", 3, '2022-04-01', 'Atividade Recuperação', 1, "D, C, F, F"),(3, "REDES-01", 1, '2021-05-23', 'Atividade Fixação', 1000, "A, C, E, C"), (4, "BD-01", 5, "2022-04-02", "Atividade MySql",  10, "A, B, C, D");
 
-INSERT INTO assuntosatividades VALUES( 2, "POO-01", "Classes", "Conceito e macetes"), ( 3, "BD-01", "Entidade Relacionamento", "Dicas de como fazer"), ( 1, "REDES-01", "Camada de Aplicação", "Conceitos"), ( 5, "BD-01", "MySql", "Instalar e atalhos");
+INSERT INTO assuntosmaterias VALUES( 2, "POO-01", "Classes", "Conceito e macetes"), ( 3, "BD-01", "Entidade Relacionamento", "Dicas de como fazer"), ( 1, "REDES-01", "Camada de Aplicação", "Conceitos"), ( 5, "BD-01", "MySql", "Instalar e atalhos");
 
 INSERT INTO atividadealunos VALUES (1, 'BielzinhoDelas', "A, A, A, A", 9), (2, 'BielzinhoDelas', "B, A, D, A", 0), (3, 'BielzinhoDelas', "A, C, E, C", 1000),  (1, 'MelrickD', "A, C, A, F", 6), (2, 'MelrickD', "D, C, F, F", 1), (3, 'MelrickD', "D, C, F, F", 890),  (1, 'LLL_Fabricio', "B, C, E, F", 10), (2, 'LLL_Fabricio', "D, C, F, F", 1), (3, 'LLL_Fabricio', "A, C, E, C", 1000), (1, 'Qemmili13', "B, C, E, F", 10), (2, 'Qemmili13', "D, C, F, F",  1), (3, 'Qemmili13', "A, C, E, A", 999);  
 
@@ -61,8 +61,6 @@ INSERT INTO conteudo VALUES('2020-01-01', "POO-01", "Apostila 12"), ('2020-01-01
 INSERT INTO videoaula VALUES('2020-01-01', "POO-01", "Classes","", 156, 'Belissima', '2021-09-30', 100), ('2023-07-09', "BD-01", "Entidade Relacionamento", "", 30, 'Amei', '2023-07-09', 900),('2022-10-01', "REDES-01", "Camada de Aplicação", "",34, 'ODEIO REDES', '2022-10-01', 2);
 
 INSERT INTO alunoturma VALUES ("BielzinhoDelas", 2022305322 , 2), ('LLL_Fabricio', 2022308, 3), ('MelrickD', 2022192023, 3), ('Qemmili13', 12341214, 2), ("BielzinhoDelas", 2022305322, 4),('LLL_Fabricio', 2022308, 2),('Qemmili13', 12341214, 3), ('Qemmili13', 12341214, 4);
-
-INSERT INTO materiaaluno VALUES("BielzinhoDelas", "POO-01"),( "BielzinhoDelas", "BD-01"), ("BielzinhoDelas", "REDES-01"), ( 'Qemmili13', "POO-01"), ('MelrickD', "REDES-01"),  ( 'MelrickD', "POO-01"), ('LLL_Fabricio', "BD-01");
 
 INSERT INTO profturma VALUES('Nenelson', 2), ("pabloDosTeclados", 4), ("dhs.lima", 3);
 
