c6fb98739e2397b458acc400c61b17316ae5d3d7
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-10-09 00:54:32 -0300

    Update instudo-BD.sql

--- 3°bimestre/instudo-BD.sql
+++ 3°bimestre/instudo-BD.sql
@@ -3,7 +3,7 @@ USE instudo;
 SET FOREIGN_KEY_CHECKS=0;
 
 CREATE TABLE alunos(
-login VARCHAR(50) NOT NULL,
+login VARCHAR(50) NOT NULL UNIQUE,
 nome VARCHAR(50) NOT NULL,
 senha VARCHAR(50) NOT NULL,
 nascimento DATE NOT NULL,
@@ -15,24 +15,24 @@ pontos INT,
 );
 
 CREATE TABLE professores(
-login VARCHAR(50) NOT NULL,
+login VARCHAR(50) NOT NULL UNIQUE,
 nome VARCHAR(50) NOT NULL,
 senha VARCHAR(50) NOT NULL,
 nascimento DATE NOT NULL,
 foto VARCHAR(50),
 especializacao VARCHAR(50) NOT NULL,
 curriculo varchar(50) NOT NULL,
-siape VARCHAR(50) NOT NULL,
+siape VARCHAR(50) NOT NULL UNIQUE,
 
 PRIMARY KEY(login)
 );
 
 CREATE TABLE empresa(
-login VARCHAR(50) NOT NULL,
+login VARCHAR(50) NOT NULL UNIQUE,
 nome VARCHAR(50) NOT NULL,
 senha VARCHAR(50) NOT NULL,
 foto VARCHAR(50) ,
-cnpj VARCHAR(18) NOT NULL,
+cnpj VARCHAR(18) NOT NULL UNIQUE,
 PRIMARY KEY(login)
 );
 
@@ -71,8 +71,8 @@ PRIMARY KEY(nomeUser)
 
 
 CREATE TABLE storys(
-idStory INT,
-conteudo VARCHAR(250),
+idStory INT NOT NULL UNIQUE,
+conteudo VARCHAR(250) NOT NULL,
 dataDeCriacao DATE,
 primary key(idStory)
 );
@@ -143,8 +143,8 @@ CREATE TABLE amizadesProfessorEmpresa(
 
 CREATE TABLE vagasVinculo(
 	FK_loginEmpresa VARCHAR(50),
-	idVaga INT,
-    descricao VARCHAR(250),
+	idVaga INT NOT NULL UNIQUE,
+    descricao VARCHAR(250) NOT NULL,
     
     FOREIGN KEY(FK_loginEmpresa) REFERENCES empresa(login),
     
@@ -152,7 +152,7 @@ CREATE TABLE vagasVinculo(
 );
 
 CREATE TABLE empresaProfessor(
-	FK_idVaga INT, #"cargo"
+	FK_idVaga INT,
 	FK_loginEmpresa VARCHAR(50),
     FK_loginProfessor VARCHAR(50),
     FOREIGN KEY(FK_loginEmpresa) REFERENCES empresa(login),
@@ -165,7 +165,7 @@ CREATE TABLE empresaProfessor(
 CREATE TABLE empresaAluno(
 	FK_loginEmpresa VARCHAR(50),
     FK_loginAluno VARCHAR(50),
-    FK_idVaga INT, #"cargo"
+    FK_idVaga INT,
     
     FOREIGN KEY(FK_loginEmpresa) REFERENCES empresa(login),
     FOREIGN KEY(FK_loginAluno) REFERENCES alunos(login),
@@ -175,7 +175,7 @@ CREATE TABLE empresaAluno(
 );
 
 CREATE TABLE posts(
-id INT UNIQUE,
+id INT NOT NULL UNIQUE,
 tipo VARCHAR(50),
 conteudo VARCHAR(50),
 comentarios VARCHAR(250),
@@ -245,8 +245,8 @@ PRIMARY KEY(FK_idPost, FK_nomeUser)
 
 
 CREATE TABLE turmas(
-ID INT,
- nome VARCHAR(50),
+ID INT NOT NULL UNIQUE,
+nome VARCHAR(50) NOT NULL,
 
 PRIMARY KEY(ID)
 );
@@ -265,7 +265,6 @@ CREATE TABLE alunoTurma(
 FK_login VARCHAR(50),
 FK_matricula INT,
 FK_IdTurma INT,
-
 FOREIGN KEY (FK_login) REFERENCES alunos(login),
 FOREIGN KEY (FK_IdTurma) REFERENCES turmas(ID),
 
@@ -273,17 +272,17 @@ PRIMARY KEY(FK_login, FK_IdTurma)
 );
 
 CREATE TABLE materias(
-IdMateria VARCHAR(50),
-nome VARCHAR(250),
+IdMateria VARCHAR(50) NOT NULL UNIQUE,
+nome VARCHAR(250) NOT NULL UNIQUE,
 
 PRIMARY KEY(IdMateria)
 );
 
 CREATE TABLE assuntosMaterias(
-Id INT,
+Id INT NOT NULL UNIQUE,
 FK_IdMateria VARCHAR(50),
-nome VARCHAR(50),
-descrição VARCHAR(250),
+nome VARCHAR(50) NOT NULL UNIQUE,
+descrição VARCHAR(250) NOT NULL,
 
 FOREIGN KEY(FK_IdMateria) REFERENCES materias(IdMateria),
 
@@ -302,17 +301,19 @@ PRIMARY KEY(FK_login, FK_IdMateria)
 
 
 CREATE TABLE atividades(
-Id INT,
+Id INT NOT NULL UNIQUE,
+FK_login VARCHAR(50),
 FK_IdMaterias VARCHAR(50),
 FK_IdAssunto INT,
-dataAtividade DATE,
-titulo VARCHAR(250),
-quantPontos INT,
-gabarito VARCHAR(50),
+dataAtividade DATE NOT NULL,
+titulo VARCHAR(250) NOT NULL,
+quantPontos INT NOT NULL,
+gabarito VARCHAR(50) NOT NULL,
 comentarios VARCHAR(250),
 
 FOREIGN KEY(FK_IdMaterias) REFERENCES materias(IdMateria),
 FOREIGN KEY(FK_IdAssunto) REFERENCES assuntosMaterias(Id),
+FOREIGN KEY(FK_login) REFERENCES professores(login),
 
 PRIMARY KEY(Id, FK_IdMaterias)
 );
@@ -333,8 +334,8 @@ PRIMARY KEY(FK_IdAtividade, FK_IdTurma)
 CREATE TABLE atividadeAlunos(
 FK_IdAtividade INT,
 FK_Login VARCHAR(50),
-resposta VARCHAR(50),
-pontosConquistados INT,
+resposta VARCHAR(50) NOT NULL,
+pontosConquistados INT NOT NULL,
 
 FOREIGN KEY (FK_IdAtividade) REFERENCES atividades(Id),
 FOREIGN KEY (FK_Login) REFERENCES alunos(login),
@@ -343,23 +344,27 @@ PRIMARY KEY(FK_IdAtividade, FK_Login)
 );
 
 CREATE TABLE conteudo(
-dtConteudo DATE,
+dtConteudo DATE NOT NULL,
 FK_IdMaterias VARCHAR(50),
-apostilas VARCHAR(50),
+FK_login VARCHAR(50),
+apostilas VARCHAR(50) NOT NULL,
 
 FOREIGN KEY(FK_IdMaterias) REFERENCES materias(IdMateria),
+FOREIGN KEY(FK_login) REFERENCES professores(login),
 PRIMARY KEY(FK_IdMaterias)
 );
 
 CREATE TABLE videoAula(
-dtConteudo DATE,
+FK_login VARCHAR(50),
+dtConteudo DATE NOT NULL,
 FK_IdMaterias VARCHAR(50),
-titulo VARCHAR(250),
-video VARCHAR(250),
-duracao INT,
+titulo VARCHAR(250) NOT NULL,
+video VARCHAR(250) NOT NULL,
+duracao INT NOT NULL,
 comentarios VARCHAR(250),
 dtComentario DATE NOT NULL,
 likes INT,
 FOREIGN KEY(FK_IdMaterias) REFERENCES materias(IdMateria),
+FOREIGN KEY(FK_login) REFERENCES professores(login),
 PRIMARY KEY(FK_IdMaterias)
 );
